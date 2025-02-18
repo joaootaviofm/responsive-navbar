0182070b43aa41e3e85884efe1ba92415678ba67
joaootaviofm <joaootaviofm@gmail.com>
2025-02-18 18:06:35 -0300

    added list items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,21 @@ export default function Navbar(){
                 <a href="#">
                     <img className="w-52 hover:scale-115 transition-all"src={logo} alt="tailwind-logo" />
                 </a>
-                <ul className='hidden md:flex gap-11 font-semibold'>
-                    <li >Home</li>
-                    <li >Products</li>
-                    <li >Explore</li>
-                    <li >Contact</li>
+                <ul className='hidden xl:flex items-center gap-12 text-base font-semibold'>
+                    <li className='hover:text-white cursor-pointer hover:bg-sky-400 p-3 rounded-md transition-all'>Home</li>
+                    <li className='hover:text-white cursor-pointer hover:bg-sky-400 p-3 rounded-md transition-all'>Products</li>
+                    <li className='hover:text-white cursor-pointer hover:bg-sky-400 p-3 rounded-md transition-all'>Explore</li>
+                    <li className='hover:text-white cursor-pointer hover:bg-sky-400 p-3 rounded-md transition-all'>Contact</li>
                 </ul>
-
+                <div className='hidden xl:flex items-center justify-center relative'>
+                    <i className='bx bx-search absolute left-3'></i>
+                    <input type="text" placeholder='Search...'
+                    className='transition-all focus:bg-slate-100 outline-sky-500 py-2 pl-10 border-2 border-sky-400 rounded-md'/>
+                </div>
+                
+                <div>
+                    <i className='bx bx-menu'></i> 
+                </div>
             </header>
         </div>
     )
