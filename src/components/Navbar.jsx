a1b3fc8761fab72dba4be4ff700a60402ee7a945
joaootaviofm <joaootaviofm@gmail.com>
2025-02-18 16:43:08 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return(
+        <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400">
+            
+        </div>
+    )
+}
