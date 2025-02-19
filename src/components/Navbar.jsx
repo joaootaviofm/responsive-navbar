24f64457479ad3d4fc52b185534d45102a64533c
joaootaviofm <joaootaviofm@gmail.com>
2025-02-19 12:01:15 -0300

    added responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import logo from '../assets/logo.png'
+import mail from '../assets/mail.svg'
 
 export default function Navbar(){
     return(
@@ -7,21 +8,28 @@ export default function Navbar(){
             <div>
                 <img className='w-40 hover:scale-115 cursor-pointer transition-all' src={logo} alt="" />
             </div>
-            <ul className='flex items-center gap-11'>
+            <ul className='hidden md:flex items-center gap-11'>
                 <li className='hover:bg-sky-500 transition-all font-bold hover:text-white p-3 rounded-lg cursor-pointer'>
                     <a href="#">Home</a>
                 </li>
                 <li className='hover:bg-sky-500 transition-all font-bold hover:text-white p-3 rounded-lg cursor-pointer'>
                     <a href="#">About</a>
                 </li>
-                <li className='hover:bg-sky-500 transition-all font-bold hover:text-white p-3 rounded-lg cursor-pointer'> 
-                    <a href="#">Projects</a>
-                </li>
-                <li className='hover:bg-sky-500 transition-all font-bold hover:text-white p-3 rounded-lg cursor-pointer'>
-                    <a href="#">Contact</a>
+                <li className='flex items-center hover:bg-sky-500 transition-all font-bold hover:text-white p-3 rounded-lg cursor-pointer'> 
+                    <a href="#">
+                        Projects
+                    </a>
                 </li>
             </ul>
-
+            <ul className='font-bold'>
+                <a href="#" className='relative flex items-center gap-x-2 hover:bg-sky-300 rounded-lg transition-all'>
+                    <li className='pl-10 transition-all hidden lg:block cursor-pointer  text-black px-7 py-3 border-1 border-transparent active:border-sky-600 active:text-white'>Contact</li>
+                    <img src={mail} className='w-7 absolute left-1'/>
+                </a>
+            </ul>
+            <div className='md:hidden'>
+                <i className='bx bx-menu text-2xl'></i>
+            </div>
         </div>
     )
 }
